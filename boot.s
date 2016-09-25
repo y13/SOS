8b4f6aeeb9d4134d7aee712c38b7092d2f2026af
eduardoscsouza <eduardoscsouza@gmail.com>
2016-09-25 17:11:08 -0300

    Etapa 1

--- boot.s
+++ boot.s
@@ -8,75 +8,17 @@ _start:
 	movl	%eax, %ebx
 	movl	%eax, %ecx
 	movl	%eax, %edx
-	movl	%eax, %ebp
-	movl	%eax, %edi
-	movl	%eax, %esi
-	movl	%eax, %esp
 	movl	%eax, %ds
 	movl	%eax, %es
 	movl	%eax, %fs
 	movl	%eax, %gs
 	movl	%eax, %ss
-	#nao muda cs pq é o program counter
 	sti
-	jmp		start
+	jmp		loop
 
-init_msg:
-	.ascii	"S.O.S - Superior Operating System!\nAlpha Version 0.0.1\0"
+loop:	
+	nop
+	jmp		loop
 
-#TO DO
-#arg1: endereco da string; #arg2: posicao para imrpimir 
-#print_string:
-
-start:
-	movb	$' ', %al
-	movb	$0x07, %ah
-
-	movl	$0xB8000, %ebx
-	movl	$0xB8FA0, %ecx
-	loop_clean:
-		movw	%ax, (%ebx)
-		addl	$2, %ebx
-		cmpl	%ebx, %ecx
-		jne	loop_clean
-
-	movl	$0xB8000, %ebx
-	movl	$init_msg, %ecx
-	loop_print:
-		movb	(%ecx), %al
-		cmpb	$0, %al
-		je 		loop_print_end
-
-		cmpb	$10, %al
-		je		loop_print_newline
-
-		movw	%ax, (%ebx)
-		addl	$2, %ebx
-		jmp		loop_print_loopback
-
-		loop_print_newline:
-			pushl	%eax
-			pushl	%edx
-			movl	%ebx, %eax
-			movl	$0xA0, %ebx
-			xorl	%edx, %edx
-
-			subl	$0xB8000, %eax
-			addl	%ebx, %eax
-			divl	%ebx
-			mull	%ebx
-			addl	$0xB8000, %eax
-
-			movl	%eax, %ebx
-			popl	%edx
-			popl	%eax
-			
-		loop_print_loopback:
-			addl	$1, %ecx
-			jmp		loop_print
-	loop_print_end:
-
-
-loop:	jmp loop
 . = _start + 510	#"magic number" para entender que é um codigo de BIOS
 .byte	0x55, 0xAA
