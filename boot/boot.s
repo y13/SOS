bc9b838ed1eaeea952578ddf335e7a1e36b1a731
eduardoscsouza <eduardoscsouza@gmail.com>
2016-09-25 19:56:10 -0300

    Implementar Clear Screen

--- boot/boot.s
+++ boot/boot.s
@@ -16,14 +16,65 @@ _start:
 	sti
 	jmp		start
 
+clear_screen:
+	pushl	%ebp
+	movl	%esp, %ebp
+
+	pushl 	%eax
+	pushl 	%ebx
+	pushl 	%ecx
+	pushl 	%edx
+
+	movb	$0x07, %ah
+	movb	$0x00, %al
+    movb    $0x07, %bh
+    movb 	$0x00, %ch
+    movb 	$0x00, %cl
+    movb 	$0x18, %dh
+    movb 	$0x4F, %dl
+    int 	$0x10 
+
+    movb	$0x02, %ah
+	movb 	$0x00, %dh
+    movb 	$0x00, %dl
+    movb    $0x00, %bh
+	int 	$0x10
+
+    popl	%edx
+    popl	%ecx
+    popl	%ebx
+    popl	%eax
+
+	pop 	%ebp
+	ret
+
 start:
+	movb	$'X', %al
+	movb	$0x07, %ah
+
+	movl	$0xB8000, %ebx
+	movl	$0xB8FA0, %ecx
+	loop_clean:
+		movw	%ax, (%ebx)
+		addl	$2, %ebx
+		cmpl	%ebx, %ecx
+		jne	loop_clean
+
+
 	loop_read_write:
 		movb	$0x00, %ah
 		int 	$0x16
 		
+		cmp		$'1', %al
+		je		clear
+		
 		movb	$0x0E, %ah
 		int 	$0x10
 		jmp		loop_read_write
+
+		clear:
+			call clear_screen
+			jmp		loop_read_write
 
 	jmp		halt
 
